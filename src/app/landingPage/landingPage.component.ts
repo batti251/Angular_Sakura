78c9907e1090fa7d8d32f368913dd080471e0ac0
batti251 <bastibuenz25@gmail.com>
2025-10-28 19:19:20 +0100

    Initiate git repo

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+//(decorater)
+@Component({ //@ ist typescriptspezifisch!
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent],
+    template: `
+    <section>
+        <div><app-navbar></app-navbar></div>
+        <h1 class="fontRaleway">SAKURA RAMEN</h1>
+        <h2>BEST RAMEN IN TOWN</h2>
+    </section>
+    `,
+    styleUrls: ['./landingPage.component.scss']
+
+}) 
+//export um es überall zur Verfügung zu stellen 
+export class LandingPageComponent{
+
+}
