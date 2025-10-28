5f8c6b946e8a84f277caf763399361ff9415bd03
batti251 <bastibuenz25@gmail.com>
2025-10-29 00:22:41 +0100

    feat[html,css]: add footer-component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 import { OurMenuComponent } from './our-menu/our-menu.component'
-import { ImprintComponent } from './../imprint/imprint.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, HowToOrderComponent, OurMenuComponent, ImprintComponent],
+  imports: [CommonModule, LandingPageComponent, HowToOrderComponent, OurMenuComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
